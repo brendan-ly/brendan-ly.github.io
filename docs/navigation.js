e09dd30c066088f613a4992ce42d5abc3f74e47f
brendan-ly <147233685+brendan-ly@users.noreply.github.com>
2023-10-07 01:27:13 -0700

    Add files via upload

---
+++ docs/navigation.js
@@ -0,0 +1,49 @@
+var pages = [
+    'snakegame.html',
+    'periodic.html',
+    'chinesezodiac/chinesezodiac.html',
+    'segments.html',
+    'basketball-3.html',
+    'personalslideshowreduced/hikingslideshow.html',
+    'speedometer/speedometer-2.html',
+    'tictactoe.html',
+    'bmi.html',
+    'color-mixer.html',
+];
+
+window.addEventListener('load', makeNavigation);
+
+function makeNavigation() {
+    // find the current page in the list 
+    var currentPage = document.location.href;
+    // alert (currentPage);
+    var index = findPage(currentPage);
+    // put the next and previous functions on the arrows
+
+    var next = (index + 1) % pages.length;
+    var previous = (index - 1 + pages.length) % pages.length;
+
+    var isInFolder = pages[index].indexOf("/") >= 0;
+
+    var links = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
+
+    if (isInFolder) {
+        links[0].href = "../" + pages[previous];
+        links[2].href = "../" + pages[next];
+    }
+    else {
+        links[0].href = pages[previous];
+        links[2].href = pages[next];
+    }
+}
+function findPage(currentPage) {
+    // alert (currentPage);
+    for (var i = 0; i < pages.length; i++) {
+        // alert (i);
+        if (currentPage.indexOf(pages[i]) >= 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
